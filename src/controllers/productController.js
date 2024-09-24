ee280ee998fd9f2d350e45d8fd6678a671d7e868
yash-babariya <yashbabariya549@gmail.com>
2024-09-24 10:49:26 +0530

    some changes....

--- src/controllers/productController.js
+++ src/controllers/productController.js
@@ -2,7 +2,7 @@ import Product from '../models/productModel.js';
 
 export const getProducts = async (req, res) => {
     try {
-        const products = await Product.find({}, '-_id -__v -createdAt -updatedAt');
+        const products = await Product.find({})
         res.json(products);
     } catch (error) {
         res.status(500).json({ message: 'Error fetching products', error: error.message });
